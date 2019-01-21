fb8c809cc8c1af51329bdd5e85ed21b16daca8d2
jross19 <jross19@stjohnsprep.org>
2019-01-20 19:37:59 -0500

    progress made on game in general

--- Don't Crash!/GameScene.swift
+++ Don't Crash!/GameScene.swift
@@ -10,8 +10,30 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    let player = SKSpriteNode(imageNamed: "yeet")
-    let enemy = SKSpriteNode(imageNamed: "spleet")
+    let player = SKSpriteNode(imageNamed: "player")
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    let gameArea: CGRect
+    
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 2.16
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size:size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func didMove(to view: SKView) {
         //deals with the background
@@ -22,16 +44,22 @@ class GameScene: SKScene {
         self.addChild(background)
         
         //deals with the player
-        player.setScale(0.3)
+        player.setScale(0.7)
         player.position = CGPoint(x: self.size.width / 2, y: self.size.height / 5)
         player.zPosition = 1
         self.addChild(player)
         
-        //deals with oncoming traffic
-        enemy.setScale(0.3)
-        enemy.position = CGPoint(x: self.size.width / 2, y: self.size.height / 1.7)
-        enemy.zPosition = 2
-        self.addChild(enemy)
+        
+        startNewLevel()
+        
+    }
+    
+    func startNewLevel() {
+        let spawn = SKAction.run(spawnEnemy)
+        let waitToSpawn = SKAction.wait(forDuration: 1)
+        let spawnSequence = SKAction.sequence([waitToSpawn, spawn])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        self.run(spawnForever)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -42,6 +70,41 @@ class GameScene: SKScene {
             let amountDragged = pointofTouch.x - previousTouch.x
             
             player.position.x += amountDragged
+            
+            //keeps the player from going too far to the right
+            if (player.position.x > gameArea.maxX - (player.size.width / 2)) {
+                player.position.x = gameArea.maxX - (player.size.width / 2)
+            }
+            
+            //keeps the player from going too far to the left
+            if (player.position.x < gameArea.minX + (player.size.width / 2)) {
+                player.position.x = gameArea.minX + (player.size.width / 2)
+            }
         }
+    }
+    
+    func spawnEnemy() {
+        let randomXStart = random(min: gameArea.minX, max: gameArea.maxX)
+        let randomXEnd = random(min: gameArea.minX, max: gameArea.maxX)
+        
+        let startPoint = CGPoint(x: randomXStart, y: self.size.height * 1.2)
+        let endPoint = CGPoint(x: randomXEnd, y: -self.size.height * 0.2)
+        
+        let enemy = SKSpriteNode(imageNamed: "enemy")
+        enemy.setScale(3)
+        enemy.position = startPoint
+        enemy.zPosition = 2
+        self.addChild(enemy)
+        
+        let moveEnemy = SKAction.move(to: endPoint, duration: 2)
+        let deleteEnemy = SKAction.removeFromParent()
+        let enemySequence = SKAction.sequence([moveEnemy, deleteEnemy])
+        enemy.run(enemySequence)
+        
+        let dx = endPoint.x - startPoint.x
+        let dy = endPoint.y - startPoint.y
+        let amountToRotate = atan2(dy, dx)
+        
+        enemy.zRotation = amountToRotate
     }
 }
