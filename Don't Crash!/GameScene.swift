283b269658794c93d18754de6c698812f7c98b44
Jonathan Ross <jross19@k215-01.stjohnsprep.org>
2019-01-23 09:11:42 -0500

    spleet

--- Don't Crash!/GameScene.swift
+++ Don't Crash!/GameScene.swift
@@ -16,7 +16,7 @@ class GameScene: SKScene {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
     
-    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
     
